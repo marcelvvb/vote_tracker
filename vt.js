2281e0e9ce5bfebc0d45a23ea465e55517454136
Marcel Buncio <marcelvvb@gmail.com>
2015-05-26 16:44:29 -0700

    update files

---
+++ vt.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+
+var kittenArray = [
+'kitten1.jpg',
+'kitten2.jpg',
+'kitten3.jpg',
+'kitten4.jpg',
+'kitten5.jpg',
+'kitten6.jpg',
+'kitten7.jpg',
+'kitten8.jpg',
+'kitten9.jpg',
+'kitten10.jpg',
+'kitten11.jpg',
+'kitten12.jpg',
+'kitten13.jpg',
+];
+
+$('<img src="images/' + kittenArray[Math.floor(Math.random() * kittenArray.length)] + '">').appendTo('#column1');
+$('<img src="images/' + kittenArray[Math.floor(Math.random() * kittenArray.length)] + '">').appendTo('#column2');
+
+});
